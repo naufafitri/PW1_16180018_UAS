a8451e7a253377132504183c5d3b5719a7f9b158
naufafitri <109568455+naufafitri@users.noreply.github.com>
2022-07-19 10:55:23 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# TutorialCI3_CRUD
+Jika teman - teman tertarik dengan pemograman, jangan lupa dukung selalu channel kami dengan cara klik tombol SUBSCRIBE serta LIKE dan SHARE video ini ke teman - teman kita yang lain https://www.youtube.com/watch?v=YdgKDLs9Owc
+
+Untuk tutorial lengkap dari project ini, teman - teman dapat melihatnya pada website resmi kami di https://blogbelajar.id/membuat-crud-create-read-update-delete-dengan-codeigniter-3-menampilkan-data-dari-database/
+
+📢 SUBSCRIBE YOUTUBE 📢
+- https://www.youtube.com/c/KecoakSaktiOfficial
+
+
+📢 FOLLOW SOCIAL MEDIA 📢
+- https://blogbelajar.id/
+- https://www.instagram.com/blogbelajar.id/
+
+
+📢 LIKE FACEBOOK FANS PAGE 📢
+- https://www.facebook.com/kecoaksaktiofficial/ (Sharing penyelesaian kasus tentang Komputer)
---
+++ crud1.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 04, 2021 at 02:42 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 5.6.37
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_mahasiswa`
+--
+
+CREATE TABLE `tb_mahasiswa` (
+  `nim` varchar(25) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `jurusan` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_mahasiswa`
+--
+
+INSERT INTO `tb_mahasiswa` (`nim`, `nama`, `jurusan`) VALUES
+('1001', 'Kecoak Sakti Officialll', 'Teknik Komputer'),
+('1002', 'Ivan Chatisa', 'Teknik Informatika');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_mahasiswa`
+--
+ALTER TABLE `tb_mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
